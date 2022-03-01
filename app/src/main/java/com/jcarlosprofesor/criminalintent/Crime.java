2817a517b1759e5ef8007dc4bb6075c38ac76feb
unknown <leo.machado.gzl@gmail.com>
2022-03-01 13:24:06 +0100

    Update comments

--- app/src/main/java/com/jcarlosprofesor/criminalintent/Crime.java
+++ app/src/main/java/com/jcarlosprofesor/criminalintent/Crime.java
@@ -1,64 +1,36 @@
 package com.jcarlosprofesor.criminalintent;
-
 import java.util.Date;
 import java.util.UUID;
 
+//Declaro la clase
 public class Crime {
 
+    //Defino los atributos de la clase
     private UUID mId;
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
-    //añadimos el campo suspect a nuestro modelo de datos
     private String mSuspect;
 
-    //Modificamos el constructor para que funcione de manera adecuada
-    //al cursor que hemos creado.
-    public Crime(){
-        this (UUID.randomUUID());
-        //mId = UUID.randomUUID();
-        //mDate = new Date();
-    }
+    //Constructor sobrecargado para el cursor
+    public Crime(){ this (UUID.randomUUID()); }
     public Crime(UUID id){
         mId = id;
         mDate = new Date();
     }
 
+    //Definimos los get de la clase
+    public UUID getId(){return mId;}
+    public String getTitle(){return mTitle;}
+    public Date getDate() {return mDate;}
+    public boolean isSolved(){return mSolved;}
+    public String getSuspect(){return mSuspect;}
+    public String getPhotoFilename(){return "IMG_"+getId().toString()+".jpg";}
 
-    public UUID getId() {
-        return mId;
-    }
+    //Definimos los set de la clase
+    public void setTitle(String title) {mTitle = title;}
+    public void setDate(Date date) {mDate = date;}
+    public void setSolved(boolean solved) {mSolved = solved;}
+    public void setSuspect(String suspect){ mSuspect = suspect;}
 
-    public String getTitle() {
-        return mTitle;
-    }
-
-    public Date getDate() {
-        return mDate;
-    }
-    public boolean isSolved(){
-        return mSolved;
-    }
-
-    public void setTitle(String title) {
-        mTitle = title;
-    }
-
-    public void setDate(Date date) {
-        mDate = date;
-    }
-
-    public void setSolved(boolean solved) {
-        mSolved = solved;
-    }
-    public String getSuspect(){
-        return mSuspect;
-    }
-    public void setSuspect(String suspect){
-        mSuspect = suspect;
-    }
-    //Metodo para obtener un nombre de fichero bien formado
-    public String getPhotoFilename(){
-        return "IMG_"+getId().toString()+".jpg";
-    }
 }
--- app/src/main/java/com/jcarlosprofesor/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/jcarlosprofesor/criminalintent/CrimeFragment.java
@@ -1,5 +1,4 @@
 package com.jcarlosprofesor.criminalintent;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -7,7 +6,6 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -24,120 +22,169 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-
 import java.io.File;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
-
 public class CrimeFragment extends Fragment {
 
+    //Declaro los widgets necesarios
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
-    //Boton de envio del informe
     private Button mReportButton;
-    //Boton para buscar el sospechoso
     private Button mSuspectButton;
-    //Boton para la camara
     private ImageButton mPhotoButton;
-    //Imagen sospechoso
     private ImageView mPhotoView;
-    //variable para la ruta de la foto
+
+    //Declaro una variable para la ruta de la foto
     private File mPhotoFile;
-    //Creamos la variable que implementara la interface.
+
+    //Creamos la variable que implementara la interface
     private Callbacks mCallbacks;
 
     /*Interface requerida para las activity que quieran albergar un fragment.*/
     public interface Callbacks{
+
         void onCrimeUpdated(Crime crime);
+
     }
-    private static  final String ARG_CRIME_ID = "crime_id";
-    //Constante que nos va a etiquetar el DataPickerFragment a mostrar
+
+    //Instancio constantes para el DataPickerFragment y el ID
+    private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
+
     //Constante para el código de petición
     private static final int REQUEST_DATE = 0;
+
     //Constante para el código de peticion del contacto
     private static final int REQUEST_CONTACT = 1;
+
     //Constante para la peticion de tomar una foto
     private static final int REQUEST_PHOTO = 2;
 
+    //Metodo para obtener el contexto tras introducir el fragment
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        mCallbacks = (Callbacks) context;
+        this.mCallbacks = (Callbacks) context;
     }
 
+    //Metodo ejecutado para desligar el fragment de la activity
     @Override
     public void onDetach() {
         super.onDetach();
-        mCallbacks = null;
+        this.mCallbacks = null;
     }
 
+    //Metodo para instanciar un fragment con un ID
     public static CrimeFragment newInstance(UUID crimeId){
         Bundle args = new Bundle();
         args.putSerializable(ARG_CRIME_ID,crimeId);
-
         CrimeFragment fragment = new CrimeFragment();
         fragment.setArguments(args);
         return fragment;
     }
 
+    //Metodo que se ejecuta segun el ciclo de vida de una fragment
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*mCrime = new Crime();*/
-        /*UUID crimeId = (UUID) getActivity().getIntent()
-                .getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);*/
+
+        //Instancio un ID obteniendo el ID del intent
         UUID crimeId = (UUID)getArguments().getSerializable(ARG_CRIME_ID);
-        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+        this.mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+
         //Instanciamos la variable para la ruta de la foto
-        mPhotoFile = CrimeLab.get(getActivity()).getPhotoFile(mCrime);
+        this.mPhotoFile = CrimeLab.get(getActivity()).getPhotoFile(mCrime);
+
     }
 
+    //Metodo que se ejecuta segun el ciclo de vida de una fragment
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //Instancio una view y la asocio a un layout
         View view = inflater.inflate(R.layout.fragment_crime, container,false);
-        //Conectamos las variables de instancia de la camara y de la imagen con sus elementos en el layout
-        mPhotoView = (ImageView)view.findViewById(R.id.crime_photo);
+
+        //Serializamos los widget
+        this.mPhotoView = (ImageView)view.findViewById(R.id.crime_photo);
+        this.mPhotoButton = (ImageButton)view.findViewById(R.id.crime_camera);
+        this.mTitleField = (EditText) view.findViewById(R.id.crime_title);
+        this.mDateButton = (Button) view.findViewById(R.id.crime_date);
+        this.mSolvedCheckBox = (CheckBox) view.findViewById(R.id.crime_solved);
+        this.mReportButton = (Button) view.findViewById(R.id.crime_report);
+        this.mSuspectButton = (Button) view.findViewById(R.id.crime_suspect);
+
+        //Obtengo la informacion del crime
+        this.mTitleField.setText(this.mCrime.getTitle());
+        this.mDateButton.setText(this.mCrime.getDate().toString());
+        this.mSolvedCheckBox.setChecked(this.mCrime.isSolved());
+        this.mCrime.setSuspect(this.mCrime.getSuspect() != null ? this.mCrime.getSuspect(): null);
+
         //Invocamos el metodo para cargar la imagen
         updatePhotoView();
-        mPhotoButton = (ImageButton)view.findViewById(R.id.crime_camera);
+
         //Creamos el intent que da tratamiento a la toma de la foto
         final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        mPhotoButton.setOnClickListener(new View.OnClickListener() {
+
+        //Codigo para dar comportamiento al boton
+        final Intent pickContact = new Intent(Intent.ACTION_PICK,ContactsContract.Contacts.CONTENT_URI);
+
+        //Defino los comportamientos de los widget mediante listener
+        this.mPhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Obtengo el file provider mediante el standard RFC 2396 y le asocio el archivo
                 Uri uri = FileProvider.getUriForFile(getActivity(),
                         "com.jcarlosprofesor.criminalintent.fileprovider",
                         mPhotoFile);
+
+                //Inserto en el intent el provider
+                //MediaStore provee una coleccion indexada de medios como audio, imagenes...
+                //Contiene definiciones para URI es el contrato entre el proveedor de medios y las aplicaciones
                 captureImage.putExtra(MediaStore.EXTRA_OUTPUT,uri);
-                List<ResolveInfo> cameraActivities = getActivity()
-                        .getPackageManager().queryIntentActivities(captureImage,
-                                PackageManager.MATCH_DEFAULT_ONLY);
-                for(ResolveInfo activity:cameraActivities){
-                    getActivity().grantUriPermission(activity.activityInfo.packageName,
-                            uri,Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+                //Informacion que es devuelta de un intent, mediante un intentfilter
+                List<ResolveInfo> cameraActivities =
+                        getActivity()
+                        .getPackageManager()//PackManager obtiene el tipo de informacion relacionada con los paquete instalados
+                        .queryIntentActivities( //Query obtiene todas las actividades que puedan realizar el intent
+                                captureImage,
+                                PackageManager.MATCH_DEFAULT_ONLY); //Establezco un flag para un package
+
+                //Recorro la lista de resolveinfo
+                for(ResolveInfo activity : cameraActivities){
+
+                    getActivity() //Contexto
+                    .grantUriPermission( //Otorgo permiso temporal a un URI
+                            activity.activityInfo.packageName, //Obtengo el nombre del paquete del resolveinfo
+                            uri,
+                            Intent.FLAG_GRANT_WRITE_URI_PERMISSION); //Flag de permiso de escritura
+
                 }
+
+                //Envio la informacion del intent y la variable asociada
                 startActivityForResult(captureImage,REQUEST_PHOTO);
+
             }
+
         });
-        mTitleField = (EditText) view.findViewById(R.id.crime_title);
-        mTitleField.setText(mCrime.getTitle());
-        mTitleField.addTextChangedListener(new TextWatcher() {
+        this.mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
 
+            //Metodo que se ejecuta cuando cambia el valor del edittext
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 mCrime.setTitle(s.toString());
@@ -148,35 +195,37 @@ public class CrimeFragment extends Fragment {
             public void afterTextChanged(Editable s) {
 
             }
-        });
 
-        mDateButton = (Button) view.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
-        //Establecemos el listener que nos mostrara el Dialog
-        mDateButton.setOnClickListener(new View.OnClickListener() {
+        });
+        this.mDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Instancio un fragmentmanager del padre
                 FragmentManager fragmentManager = getParentFragmentManager();
+
+                //Instancio un datepicker mediante la fecha del crime
                 DatePickerFragment dialog = DatePickerFragment.newInstante(mCrime.getDate());
+
                 //Establecemos el fragment destino
                 dialog.setTargetFragment(CrimeFragment.this,REQUEST_DATE);
+
+                //Muestro el datepicker
                 dialog.show(fragmentManager,DIALOG_DATE);
             }
-        });
 
-        mSolvedCheckBox = (CheckBox) view.findViewById(R.id.crime_solved);
-        mSolvedCheckBox.setChecked(mCrime.isSolved());
-        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        });
+        this.mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                //Cambio la variable del crime y lo actualizo
                 mCrime.setSolved(isChecked);
                 updateCrime();
             }
+
         });
-        //Creamos una referencia para el boton de envio del informe
-        //En la implementación de su listener añadimos el intent implicito de envio de text
-        mReportButton = (Button) view.findViewById(R.id.crime_report);
-        mReportButton.setOnClickListener(new View.OnClickListener() {
+        this.mReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_SEND);
@@ -190,78 +239,113 @@ public class CrimeFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        //Codigo para dar comportamiento al boton
-        final Intent pickContact = new Intent(Intent.ACTION_PICK,
-                ContactsContract.Contacts.CONTENT_URI);
-        //pickContact.addCategory(Intent.CATEGORY_HOME);
-        mSuspectButton = (Button) view.findViewById(R.id.crime_suspect);
-        mSuspectButton.setOnClickListener(new View.OnClickListener() {
+        this.mSuspectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //Envio el intent y el valor de su variable asociada
                 startActivityForResult(pickContact,REQUEST_CONTACT);
+
             }
+
         });
-        if(mCrime.getSuspect()!=null){
-            mSuspectButton.setText(mCrime.getSuspect());
-        }
-        //Comprobamos que las activity responden a los intent
-       /* PackageManager packageManager = getActivity().getPackageManager();
-        if(packageManager.resolveActivity(pickContact,
-                PackageManager.MATCH_DEFAULT_ONLY) == null){
-            mSuspectButton.setEnabled(false);
-        }*/
+
+        //Retorno la view
         return view;
+
     }
+
     //Sobreescribimos el método onActivityResult para recuperar el extra, fijamos la fecha en el objeto Crime
     //y actualizamos el texto del boton
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        //
         if(resultCode != Activity.RESULT_OK){
             return;
         }
+
+        //
         if (requestCode == REQUEST_DATE){
-            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
-            mCrime.setDate(date);
+
+            //
+            this.mCrime.setDate((Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE));
+
+            //
             updateCrime();
-            mDateButton.setText(mCrime.getDate().toString());
+
+            //
+            this.mDateButton.setText(mCrime.getDate().toString());
+
         }
+
         //añadimos el tratamiento del resultado devuelto por la app Contactos
         else if (requestCode == REQUEST_CONTACT && data != null){
+
+            //
             Uri contactUri = data.getData();
+
             //Especificamos el campo para el que queremos que la consulta
             //devuelva valores
             String[] queryFields = new String[]{
                     ContactsContract.Contacts.DISPLAY_NAME
             };
+
             //Ejecutamos la consulta
             Cursor c = getActivity().getContentResolver().query(
-                    contactUri,queryFields,null,null,null);
+                    contactUri,
+                    queryFields,
+                    null,
+                    null,
+                    null);
+
             try{
+
                 //Comprobamos que hemos obtenidos resultados
-                if(c.getCount() == 0){
-                    return;
-                }
+                if(c.getCount() == 0){ return;}
+
                 //Extraemos la primera columna
                 //Es el nombre del sospechoso
                 c.moveToFirst();
+
+                //
                 String suspect = c.getString(0);
-                mCrime.setSuspect(suspect);
+
+                //
+                this.mCrime.setSuspect(suspect);
+
+                //
                 updateCrime();
-                mSuspectButton.setText(suspect);
+
+                //
+                this.mSuspectButton.setText(suspect);
+
             }finally {
+
+                //
                 c.close();
+
             }
-        }else if (requestCode == REQUEST_PHOTO){
+
+        //
+        } else if (requestCode == REQUEST_PHOTO){
+
+            //
             Uri uri = FileProvider.getUriForFile(getActivity(),
                     "com.jcarlosprofesor.criminalintent.fileprovider",
-                    mPhotoFile);
+                    this.mPhotoFile);
 
-            getActivity().revokeUriPermission(uri,
-                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            //
+            getActivity().revokeUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+            //
             updateCrime();
+
+            //
             updatePhotoView();
+
         }
+
     }
 
     //Sobreescribimos el metodo onPause para asegurarnos que las instancias
@@ -279,7 +363,7 @@ public class CrimeFragment extends Fragment {
     //metodo que nos va a construir el informe de un crime concreto en ejecucion
     private String getCrimeReport(){
         String solvedString = null;
-        if(mCrime.isSolved()){
+        if(this.mCrime.isSolved()){
             solvedString = getString(R.string.crime_report_solved);
         }
         else{
@@ -298,18 +382,29 @@ public class CrimeFragment extends Fragment {
                 mCrime.getTitle(),dateString,solvedString,suspect);
         return report;
     }
+
     //metodo para cargat el objeto Bitmap en el ImageView
     private void updatePhotoView(){
-        if(mPhotoFile == null || !mPhotoFile.exists()){
+
+        //
+        if(this.mPhotoFile == null || !this.mPhotoFile.exists()){
+
             mPhotoView.setImageDrawable(null);
+
         }else{
+
+            //
             Bitmap bitmap = PictureUtils.getScaledBitmap(
-                    mPhotoFile.getPath(), getActivity());
-            mPhotoView.setImageBitmap(bitmap);
+                    this.mPhotoFile.getPath(), getActivity());
+            this.mPhotoView.setImageBitmap(bitmap);
         }
     }
+
+    //
     private void updateCrime(){
-        CrimeLab.get(getActivity()).updateCrime(mCrime);
-        mCallbacks.onCrimeUpdated(mCrime);
+
+        CrimeLab.get(getActivity()).updateCrime(this.mCrime);
+        this.mCallbacks.onCrimeUpdated(this.mCrime);
+
     }
 }
